de54e53d1b72250a4045b8e7074cf4dd0d3a9691
evingulcan <56730736+evingulcan@users.noreply.github.com>
2022-02-22 11:43:05 +0300

    text

--- lib/product/companent/app_text_constant.dart
+++ lib/product/companent/app_text_constant.dart
@@ -6,7 +6,7 @@ class AppString {
   static const String login = "Design Relish";
   static const String loginPage = "LOGIN";
 
-  static const String haveAccount = "Don't you have an account ? Create";
+  static const String haveAccount = "Don't you have an account ? Create!";
   static const String email = 'Email Adress';
   static const String password = 'Password';
   static const String forgot = "Forgot Password?";
